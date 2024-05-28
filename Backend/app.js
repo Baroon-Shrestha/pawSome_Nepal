1ec2ef5a6ecde89f9b0a29776367bbf553819393
Baroon-Shrestha <119175371+Baroon-Shrestha@users.noreply.github.com>
2024-05-28 21:02:05 +0545

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,9 @@ const app = Express();
 
 dotenv.config({ path: "./config/config.env" });
 
-app.use(cors());
+app.use(cors({
+    origin: "https://pawsome-nepal.vercel.app",
+}));
 
 app.use(cookieParser())
 app.use(Express.json());
